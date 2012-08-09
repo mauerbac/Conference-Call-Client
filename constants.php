dd4108b274abe53cf2599d1dcf2e399781b0b9fc
Matt Auerbach <tennisfan0526@gmail.com>
2012-08-09 10:41:27 -0400

    upload

---
+++ constants.php
@@ -0,0 +1,19 @@
+<?
+/**
+ * constants.php
+ */
+ 
+//Google Oauth Credentials: https://code.google.com/apis/console/
+define("APPLICATION_NAME", "xxx"); //Enter Your Application Name
+define("CLIENT_ID", "xxx"); //Enter your ClientId
+define("CLIENT_SECRET", "xxx");  // Enter your ClientSecret
+define("REDIRECT_URI", "xxx"); //Enter setRedirectUri 
+define("DEVELOPER_KEY", "xxx");   //Enter your developer Key
+
+//Twilio Credentials: https://www.twilio.com/user/account
+define("ACCOUNT_SID", "xxx"); //Enter Your Twilio Account Sid
+define("TAUTH_TOKEN", "xxx"); //Enter your Twilio Auth Token
+define("APP_ID", "xxx");  // Enter your TwiML App Id
+
+
+?>
